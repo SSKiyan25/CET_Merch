6566f2d7de617b7b524e5f3b5fec5d7f36cc56e5
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-03-16 21:36:42 +0800

    Update NavigationView.vue

--- src/components/navigation/views/NavigationView.vue
+++ src/components/navigation/views/NavigationView.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="w-full fixed bg-secondary border-accent md:px-10 border-b-2 z-40">
+  <nav
+    class="w-full fixed top-0 bg-secondary border-accent md:px-5 border-b-2 z-40"
+  >
     <div class="flex items-center justify-between p-3">
       <div class="flex">
         <router-link
@@ -12,7 +14,11 @@
           >
         </router-link>
       </div>
-      <div class="flex md:order-2 md:space-x-0 rtl:space-x-reverse">
+      <!--Sign In/Up Icons-->
+      <div
+        v-if="!user"
+        class="flex md:order-2 md:space-x-0 rtl:space-x-reverse"
+      >
         <div class="flex-auto justify-end md:space-x-1">
           <router-link
             to="/login"
@@ -28,13 +34,136 @@
           </router-link>
         </div>
       </div>
+      <!--User Dropdown-->
+      <div
+        v-else
+        class="relative flex justify-between md:order-2 items-center w-[6rem]"
+      >
+        <div class="flex">
+          <button>
+            <span class="material-symbols-outlined text-2xl">
+              shopping_cart
+            </span>
+          </button>
+        </div>
+        <div class="flex items-center ms-3">
+          <div>
+            <button
+              type="button"
+              @click.prevent="toggleUserDropdown"
+              class="flex text-sm bg-secondary border-accent rounded-full focus:ring-4 focus:ring-accent"
+              :aria-expanded="dropdownUserVisible"
+            >
+              <span class="sr-only">Open user menu</span>
+              <img
+                class="w-8 h-8 rounded-full saturate-0"
+                src="/avatar.png"
+                alt="user photo"
+                style="filter: brightness(0) invert(1)"
+              />
+            </button>
+          </div>
+        </div>
+
+        <div
+          :class="{ hidden: !dropdownUserVisible }"
+          class="absolute z-50 mt-56 -ml-12 text-base list-none divide-y divide-primary/10 rounded shadow bg-secondary border-accent"
+          id="dropdown-user"
+        >
+          <div class="px-4 py-3">
+            <p class="text-sm dark:text-white">{{ username }}</p>
+            <p
+              class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
+            >
+              {{ userEmail }}
+            </p>
+          </div>
+          <ul class="py-1">
+            <li>
+              <router-link
+                to="/admin"
+                class="block px-4 py-2 text-sm text-muted-foreground hover:bg-primary/20"
+                role="menuitem"
+                >Dashboard
+              </router-link>
+            </li>
+            <li>
+              <a
+                class="block px-4 py-2 text-sm text-muted-foreground hover:bg-primary/20 pointer-events-auto cursor-pointer"
+                role="menuitem"
+              >
+                Settings
+              </a>
+            </li>
+            <li>
+              <a
+                @click.prevent="handleSignout"
+                class="block px-4 py-2 text-sm text-muted-foreground hover:bg-primary/20 pointer-events-auto cursor-pointer"
+                role="menuitem"
+              >
+                Sign out
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { initFlowbite } from "flowbite";
+import { auth } from "@/firebase/init.ts";
+import type { User } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebase/init.ts";
+import { useRouter } from "vue-router";
+import { signOut } from "firebase/auth";
+
+const router = useRouter();
+const user = ref<User | null>(null);
+const userEmail = ref("");
+const username = ref("");
+
+const dropdownUserVisible = ref(false);
+
+const toggleUserDropdown = () => {
+  dropdownUserVisible.value = !dropdownUserVisible.value;
+};
+
+auth.onAuthStateChanged(async (currentUser) => {
+  if (currentUser) {
+    user.value = currentUser;
+    userEmail.value = currentUser.email || "";
+
+    // Fetch the user's document from Firestore
+    const userDoc = doc(db, "users", currentUser.uid);
+    const userSnap = await getDoc(userDoc);
+
+    if (userSnap.exists()) {
+      // The document exists, update the user data
+      username.value = userSnap.data().username || "";
+    } else {
+      console.error("No such document!");
+    }
+  } else {
+    user.value = null;
+    userEmail.value = "";
+    username.value = "";
+  }
+});
+
+const handleSignout = () => {
+  signOut(auth)
+    .then(() => {
+      console.log("User signed out");
+      router.push({ name: "login" });
+    })
+    .catch((error) => {
+      console.log("Error signing out: ", error);
+    });
+};
 
 onMounted(() => {
   initFlowbite();
