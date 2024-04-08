5cf89a7b15cd8289821859cc1c1bcb1d2b2deb27
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-04-08 21:53:25 +0800

    Update UserSidebarView.vue

--- src/components/feature/user/userDashboard/views/UserSidebarView.vue
+++ src/components/feature/user/userDashboard/views/UserSidebarView.vue
@@ -37,37 +37,6 @@
             <span class="ms-3">Profile</span>
           </router-link>
         </li>
-
-        <li>
-          <router-link
-            to="/dashboard"
-            class="flex items-center p-2 text-secondary-foreground rounded-lg hover:bg-primary/10 group"
-            :class="{
-              'text-white hover:text-primary bg-primary/80':
-                $route.path === '/',
-            }"
-          >
-            <svg
-              class="flex-shrink-0 w-5 h-5 text-secondary-foreground transition duration-75 group-hover:text-primary"
-              :class="{
-                'text-white': $route.path === '/',
-              }"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"
-              />
-            </svg>
-            <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
-            <span
-              class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-primary-foreground bg-primary rounded-full"
-              >3</span
-            >
-          </router-link>
-        </li>
         <li>
           <router-link
             to="/dashboard/orders"
