438388b6de18f5c8af8c7dd26445a13b3136bc67
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-04-09 16:53:52 +0800

    Update HeaderProductsComponent.vue

--- src/components/feature/dashboard/components/HeaderProductsComponent.vue
+++ src/components/feature/dashboard/components/HeaderProductsComponent.vue
@@ -3,7 +3,7 @@
     <div class="w-full pt-4">
       <div class="grid grid-cols-3 gap-1 md:gap-2">
         <div
-          class="group bg-gradient-to-br from-gold to-light-gold h-20 w-auto md:h-[12rem] md:w-full shadow-lg rounded-sm"
+          class="group bg-gradient-to-br from-gold to-light-gold h-20 w-auto md:h-[12rem] md:w-full shadow-lg rounded-sm hover:drop-shadow-2xl hover:contrast-125"
         >
           <div
             class="flex flex-row border-primary/60 justify-start items-start pl-1 md:pl-4 pt-5"
@@ -11,7 +11,7 @@
             <div class="">
               <img
                 src="/homepage/about-1.jpg"
-                class="md:w-48 md:h-36 object-cover brightness-90 rounded-lg"
+                class="md:w-48 md:h-36 object-cover brightness-90 rounded-sm drop-shadow-xl"
               />
             </div>
             <div class="flex flex-col -space-y-1 md:space-y-2">
@@ -29,7 +29,7 @@
           </div>
         </div>
         <div
-          class="group bg-gradient-to-br from-gold to-light-gold h-20 w-auto md:h-[12rem] md:w-full shadow-lg rounded-sm"
+          class="group bg-gradient-to-br from-gold to-light-gold h-20 w-auto md:h-[12rem] md:w-full shadow-lg rounded-sm hover:drop-shadow-2xl hover:contrast-125"
         >
           <div
             class="flex flex-row border-primary/60 justify-start items-start max-h-[22rem] pl-1 md:pl-4 pt-5"
@@ -37,7 +37,7 @@
             <div class="">
               <img
                 src="/homepage/products-1.jpg"
-                class="md:w-48 md:h-36 object-cover brightness-90 rounded-lg shadow-xl"
+                class="md:w-48 md:h-36 object-cover brightness-90 rounded-sm drop-shadow-xl"
               />
             </div>
             <div class="flex flex-col -space-y-1 md:space-y-2">
@@ -59,7 +59,7 @@
           </div>
         </div>
         <div
-          class="group bg-gradient-to-br from-gold to-light-gold h-20 w-auto md:h-[12rem] md:w-full shadow-lg rounded-sm"
+          class="group bg-gradient-to-br from-gold to-light-gold h-20 w-auto md:h-[12rem] md:w-full shadow-lg rounded-sm hover:drop-shadow-2xl hover:contrast-125"
         >
           <div
             class="flex flex-row border-primary/60 justify-start items-start max-h-[22rem] pl-1 md:pl-4 pt-5"
@@ -67,7 +67,7 @@
             <div class="w-12 h-28 md:w-48 md:h-64">
               <img
                 src="/homepage/gallery-1.jpg"
-                class="md:w-48 md:h-36 object-cover brightness-90 rounded-lg shadow-xl"
+                class="md:w-48 md:h-36 object-cover brightness-90 rounded-sm drop-shadow-xl"
               />
             </div>
             <div class="flex flex-col -space-y-1 md:space-y-2">
