67901537477c82592b672e965e43d71ae821afc1
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-04-10 23:03:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 import tailwind from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   css: {
     postcss: {
@@ -17,4 +16,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: process.env.NODE_ENV === "production" ? "/CS_Shoppee/" : "/",
 });
