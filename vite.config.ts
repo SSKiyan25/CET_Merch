f6dcc8eece2ee870704f61fd458492c615c9811a
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-04-11 00:20:54 +0800

    trying to fix production - v6

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: [".js", ".ts", ".json", ".jsx", ".mjs", ".tsx", ".vue"],
   },
   base: "/CS_Shoppee/",
 });
