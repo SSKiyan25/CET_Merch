1eca8115d8d4c389bf87f9372a180891a7194e6b
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-04-15 16:17:22 +0800

    changed quantity input design

--- src/components/feature/user/userOrder/views/AddToCartView.vue
+++ src/components/feature/user/userOrder/views/AddToCartView.vue
@@ -180,15 +180,64 @@
 
             <div class="flex flex-row pt-4">
               <span class="pt-2.5 pr-2 text-xs md:text-sm"> Quantity : </span>
-              <Input
-                type="number"
-                min="1"
-                max="1000"
-                placeholder="0"
-                class="px-2 py-2 w-1/6 text-xs md:text-base text-center"
-                v-model="newAddToCartData.quantity"
-                required
-              />
+              <div class="relative flex items-center max-w-[8rem]">
+                <button
+                  type="button"
+                  id="decrement-button"
+                  @click.prevent="decrement"
+                  data-input-counter-decrement="quantity-input"
+                  class="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+                >
+                  <svg
+                    class="w-3 h-3 text-gray-900"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 2"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M1 1h16"
+                    />
+                  </svg>
+                </button>
+                <input
+                  type="text"
+                  id="quantity-input"
+                  data-input-counter
+                  aria-describedby="helper-text-explanation"
+                  class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5"
+                  placeholder="999"
+                  v-model="newAddToCartData.quantity"
+                  required
+                />
+                <button
+                  type="button"
+                  id="increment-button"
+                  @click.prevent="increment"
+                  data-input-counter-increment="quantity-input"
+                  class="bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none"
+                >
+                  <svg
+                    class="w-3 h-3 text-gray-900 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 18 18"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M9 1v16M1 9h16"
+                    />
+                  </svg>
+                </button>
+              </div>
               <span class="pt-2.5 px-2 text-xs md:text-sm"
                 >Initial Price :</span
               >
@@ -360,6 +409,9 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
+import { initFlowbite } from "flowbite";
+
+const { handleAddToCartSubmit, newAddToCartData } = setupAddToCartController();
 
 const router = useRouter();
 const orderData = ref<OrderDataType[] | null>(null);
@@ -398,7 +450,20 @@ const props = defineProps({
 
 //console.log("productId in Cart component: ", props.productId);
 
+// Define the increment function
+const increment = () => {
+  newAddToCartData.value.quantity++;
+};
+
+// Define the decrement function
+const decrement = () => {
+  if (newAddToCartData.value.quantity > 0) {
+    newAddToCartData.value.quantity--;
+  }
+};
+
 onMounted(async () => {
+  initFlowbite();
   const results = await getOnQueueOrdersController();
   if (results.length > 0) {
     const onQueueOrders = results.filter(
@@ -419,8 +484,6 @@ const injectedGetProductById = inject("getProductById") as (
 ) => ProductType;
 const product = computed(() => injectedGetProductById(props.productId));
 //console.log("Product data: ", product.value);
-
-const { handleAddToCartSubmit, newAddToCartData } = setupAddToCartController();
 
 let isLoading = ref(false);
 let isUploadSuccessful = ref(false);
--- src/components/feature/user/userOrder/views/ConfirmationOrderView.vue
+++ src/components/feature/user/userOrder/views/ConfirmationOrderView.vue
@@ -1,150 +1,157 @@
 <template>
-  <div class="flex pt-12 md:pt-24 pb-36 justify-center">
-    <div
-      class="p-7 w-full md:w-3/4 lg:w-5/6 items-center justify-center bg-accent border border-primary/20 rounded-xl shadow-sm overflow-auto"
-    >
-      <div class="p-2 border-secondary-foreground/40">
-        <div class="flex flex-row justify-between">
-          <div class="flex flex-row text-start">
-            <span
-              class="material-symbols-outlined text-2xl pr-2 text-primary/90"
+  <div class="h-screen">
+    <div class="flex pt-12 md:pt-24 pb-36 justify-center">
+      <div
+        class="p-7 w-full md:w-3/4 lg:w-5/6 items-center justify-center bg-accent border border-primary/20 rounded-xl shadow-sm overflow-auto"
+      >
+        <div class="p-2 border-secondary-foreground/40">
+          <div class="flex flex-row justify-between">
+            <div class="flex flex-row text-start">
+              <span
+                class="material-symbols-outlined text-2xl pr-2 text-primary/90"
+              >
+                shopping_basket
+              </span>
+              <h1 class="block text-2xl font-bold text-primary/90">
+                Your Cart
+              </h1>
+            </div>
+            <div class="flex flex-row items-center justify-center">
+              <router-link :to="`/submitorder/${route.params.id}`">
+                <button
+                  class="p-2 bg-emerald-800 rounded-sm hover:bg-emerald-700 text-xs md:text-base text-background font-semibold flex items-center"
+                >
+                  Process Order
+                  <span class="material-symbols-outlined ml-2"> start </span>
+                </button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <hr class="w-auto" />
+        <div class="pt-2" v-if="!loading">
+          <div class="md:p-2 bg-backgound">
+            <table class="text-secondary-foreground w-full overflow-auto">
+              <thead>
+                <tr
+                  class="uppercase md:tracking-wide text-[10px] md:text-sm text-primary/90"
+                >
+                  <th class="p-2 text-left md:w-2/12"></th>
+                  <th class="p-2 text-left md:w-3/12">Product Name</th>
+                  <th class="p-2 text-left">Size</th>
+                  <th class="p-2 text-left">Quantity</th>
+                  <th class="p-2 text-left">Price</th>
+                  <th class="p-2 text-left">Total Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(product, index) in cart"
+                  :key="index"
+                  class="border-b border-primary/40 text-[10px] md:text-sm hover:bg-primary/10"
+                >
+                  <td class="p-2">
+                    <img
+                      class="md:w-40 md:h-40 rounded-sm"
+                      :src="product.details.coverPhoto"
+                      :alt="product.name"
+                    />
+                  </td>
+                  <td class="p-2 text-wrap whitespace-normal">
+                    {{ product.details?.name }}
+                  </td>
+                  <td class="p-2 md:pl-4">{{ product.size }}</td>
+                  <td class="p-2 pl-7 md:pl-10">{{ product.quantity }}</td>
+                  <td class="p-2 md:pl-4">
+                    {{
+                      product.details.price[product.details.price.length - 1]
+                        .originalPrice
+                    }}
+                  </td>
+                  <td class="p-2 md:pl-8">
+                    {{
+                      product.details?.price[product.details.price.length - 1]
+                        .originalPrice * product.quantity
+                    }}
+                  </td>
+                  <td class="p-2">
+                    <AlertDialog>
+                      <AlertDialogTrigger
+                        ><button
+                          class="material-symbols-outlined text-white bg-primary p-2 rounded-sm"
+                        >
+                          delete
+                        </button>
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            Delete Product to Cart
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            <p class="text-black">
+                              Are you sure you want to delete this product from
+                              your cart? This action cannot be undone.
+                            </p>
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <div class="flex flex-row justify-end space-x-2">
+                            <AlertDialogAction
+                              class="bg-background pb-2 hover:bg-slate-200 text-black"
+                            >
+                              Cancel
+                            </AlertDialogAction>
+                            <AlertDialogAction
+                              class="bg-primary hover:bg-destructive/80 text-white"
+                              @click.prevent="deleteProductController(index)"
+                            >
+                              Delete
+                            </AlertDialogAction>
+                          </div>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="pt-8">
+          <div class="flex flex-row justify-between">
+            <div
+              class="flex flex-row items-center justify-center space-x-2 pl-2"
             >
-              shopping_basket
-            </span>
-            <h1 class="block text-2xl font-bold text-primary/90">Your Cart</h1>
-          </div>
-          <div class="flex flex-row items-center justify-center">
-            <router-link :to="`/submitorder/${route.params.id}`">
-              <button
-                class="p-2 bg-emerald-800 rounded-sm hover:bg-emerald-700 text-xs md:text-base text-background font-semibold flex items-center"
-              >
-                Process Order
-                <span class="material-symbols-outlined ml-2"> start </span>
-              </button>
-            </router-link>
-          </div>
-        </div>
-      </div>
-      <hr class="w-auto" />
-      <div class="pt-2" v-if="!loading">
-        <div class="md:p-2 bg-backgound">
-          <table class="text-secondary-foreground w-full overflow-auto">
-            <thead>
-              <tr
-                class="uppercase md:tracking-wide text-[10px] md:text-sm text-primary/90"
-              >
-                <th class="p-2 text-left md:w-2/12"></th>
-                <th class="p-2 text-left md:w-3/12">Product Name</th>
-                <th class="p-2 text-left">Size</th>
-                <th class="p-2 text-left">Quantity</th>
-                <th class="p-2 text-left">Price</th>
-                <th class="p-2 text-left">Total Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="(product, index) in cart"
-                :key="index"
-                class="border-b border-primary/40 text-[10px] md:text-sm hover:bg-primary/10"
-              >
-                <td class="p-2">
-                  <img
-                    class="md:w-40 md:h-40 rounded-sm"
-                    :src="product.details.coverPhoto"
-                    :alt="product.name"
-                  />
-                </td>
-                <td class="p-2 text-wrap whitespace-normal">
-                  {{ product.details?.name }}
-                </td>
-                <td class="p-2 md:pl-4">{{ product.size }}</td>
-                <td class="p-2 pl-7 md:pl-10">{{ product.quantity }}</td>
-                <td class="p-2 md:pl-4">
-                  {{
-                    product.details.price[product.details.price.length - 1]
-                      .originalPrice
-                  }}
-                </td>
-                <td class="p-2 md:pl-8">
-                  {{
-                    product.details?.price[product.details.price.length - 1]
-                      .originalPrice * product.quantity
-                  }}
-                </td>
-                <td class="p-2">
-                  <AlertDialog>
-                    <AlertDialogTrigger
-                      ><button
-                        class="material-symbols-outlined text-white bg-primary p-2 rounded-sm"
-                      >
-                        delete
-                      </button>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>
-                          Delete Product to Cart
-                        </AlertDialogTitle>
-                        <AlertDialogDescription>
-                          <p class="text-black">
-                            Are you sure you want to delete this product from
-                            your cart? This action cannot be undone.
-                          </p>
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <AlertDialogFooter>
-                        <div class="flex flex-row justify-end space-x-2">
-                          <AlertDialogAction
-                            class="bg-background pb-2 hover:bg-slate-200 text-black"
-                          >
-                            Cancel
-                          </AlertDialogAction>
-                          <AlertDialogAction
-                            class="bg-primary hover:bg-destructive/80 text-white"
-                            @click.prevent="deleteProductController(index)"
-                          >
-                            Delete
-                          </AlertDialogAction>
-                        </div>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialog>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <div class="pt-8">
-        <div class="flex flex-row justify-between">
-          <div class="flex flex-row items-center justify-center space-x-2 pl-2">
-            <router-link to="/"
-              ><button
-                class="bg-primary text-white text-xs md:text-sm rounded-sm p-2"
-              >
-                Back
-              </button>
-            </router-link>
-            <router-link :to="`/submitorder/${route.params.id}`">
-              <button
-                class="p-2 bg-emerald-800 rounded-sm hover:bg-emerald-700 text-xs md:text-base text-background font-semibold flex items-center"
-              >
-                Process Order
-                <span class="material-symbols-outlined ml-2"> start </span>
-              </button>
-            </router-link>
-          </div>
+              <router-link to="/"
+                ><button
+                  class="bg-primary text-white text-xs md:text-sm rounded-sm p-2"
+                >
+                  Back
+                </button>
+              </router-link>
+              <router-link :to="`/submitorder/${route.params.id}`">
+                <button
+                  class="p-2 bg-emerald-800 rounded-sm hover:bg-emerald-700 text-xs md:text-base text-background font-semibold flex items-center"
+                >
+                  Process Order
+                  <span class="material-symbols-outlined ml-2"> start </span>
+                </button>
+              </router-link>
+            </div>
 
-          <div class="flex flex-row justify-end p-2">
-            <span class="text-lg font-bold pr-2">Total:</span>
-            <span class="text-lg font-bold underline text-primary/90"
-              >P{{ totalPrice }}
-            </span>
+            <div class="flex flex-row justify-end p-2">
+              <span class="text-lg font-bold pr-2">Total:</span>
+              <span class="text-lg font-bold underline text-primary/90"
+                >P{{ totalPrice }}
+              </span>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+
   <div
     v-if="loading"
     class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-80"
