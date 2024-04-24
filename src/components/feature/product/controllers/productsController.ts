a25f157dd8a27134fbc7462d13031e29a200e0ca
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-04-24 11:21:53 +0800

    fixed product sizes in user view

--- src/components/feature/product/controllers/productsController.ts
+++ src/components/feature/product/controllers/productsController.ts
@@ -39,7 +39,7 @@ export const setup = () => {
             // Iterate over each item in the size array
             for (const sizeItem of sizeArray) {
               // Check if remaining_stocks is not zero
-              if (Number(sizeItem.remaining_stocks) > 0) {
+              if (Number(sizeItem.price) > 0) {
                 // Return the price of this item
                 return sizeItem.price;
               }
