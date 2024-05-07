fd978a88930817b14e7d73ca37af462ec594f146
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-05-07 11:52:17 +0800

    Update BestSellingProductsComponent.vue

--- src/components/feature/dashboard/components/BestSellingProductsComponent.vue
+++ src/components/feature/dashboard/components/BestSellingProductsComponent.vue
@@ -22,7 +22,7 @@
               </p>
             </div>
           </div>
-          <div class="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <div class="grid grid-cols-2 md:grid-cols-4 gap-6">
             <div
               v-if="!isLoading"
               v-for="product in products"
